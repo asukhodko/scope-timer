9744ecd4c18f3834e3c6603d3ad17c45ea51fdbe
Sukhodko Aleksadr <asukhodko@gmail.com>
2017-07-04 22:10:04 +0300

    Scope moved out from ScopeTimer

---
+++ ScopeTimer/Scope.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ScopeTimer
+{
+    public class Scope : IDisposable
+    {
+        private readonly ScopeTimer _scopeTimer;
+
+        private readonly Stopwatch _sw;
+
+        public Scope(string scopeName)
+        {
+            _scopeTimer = new ScopeTimer(scopeName);
+            _sw = new Stopwatch();
+            _sw.Start();
+        }
+
+        public ScopeTimer ScopeTimer
+        {
+            get
+            {
+                _scopeTimer.Elapsed = _sw.Elapsed;
+                return _scopeTimer;
+            }
+        }
+
+        public void Dispose()
+        {
+            _sw.Stop();
+            _scopeTimer.Elapsed = _sw.Elapsed;
+        }
+
+        public Scope Inner(string scopeName)
+        {
+            var scopeTimerBuilder = new Scope(scopeName);
+            _scopeTimer.InnerScopes.Add(scopeTimerBuilder._scopeTimer);
+            return scopeTimerBuilder;
+        }
+    }
+}
--- ScopeTimer/ScopeTimer.cs
+++ ScopeTimer/ScopeTimer.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 
 namespace ScopeTimer
 {
@@ -12,46 +11,10 @@ namespace ScopeTimer
             ScopeName = scopeName;
         }
 
-        public TimeSpan Elapsed { get; private set; }
+        public TimeSpan Elapsed { get; internal set; }
 
         public List<ScopeTimer> InnerScopes { get; }
 
         public string ScopeName { get; }
-
-        public class Scope : IDisposable
-        {
-            private readonly ScopeTimer _scopeTimer;
-
-            private readonly Stopwatch _sw;
-
-            public Scope(string scopeName)
-            {
-                _scopeTimer = new ScopeTimer(scopeName);
-                _sw = new Stopwatch();
-                _sw.Start();
-            }
-
-            public ScopeTimer ScopeTimer
-            {
-                get
-                {
-                    _scopeTimer.Elapsed = _sw.Elapsed;
-                    return _scopeTimer;
-                }
-            }
-
-            public void Dispose()
-            {
-                _sw.Stop();
-                _scopeTimer.Elapsed = _sw.Elapsed;
-            }
-
-            public Scope Inner(string scopeName)
-            {
-                var scopeTimerBuilder = new Scope(scopeName);
-                _scopeTimer.InnerScopes.Add(scopeTimerBuilder._scopeTimer);
-                return scopeTimerBuilder;
-            }
-        }
     }
 }
